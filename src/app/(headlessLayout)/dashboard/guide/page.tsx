5fa2699d2e1f72cb6c0382200cfc06a5ae1a01ff
MrDalo <dadokralik@gmail.com>
2023-11-22 17:51:12 +0100

    feat: Finished user auth with discord/Google | Handled all types of red.

--- src/app/(headlessLayout)/dashboard/guide/page.tsx
+++ src/app/(headlessLayout)/dashboard/guide/page.tsx
@@ -3,12 +3,12 @@ import { redirect } from 'next/navigation';
 import React from 'react';
 
 const DashboardPage = async () => {
-	const status = await getServerAuthSession();
-	console.log(status);
-	if (!status) {
-		// User unauthenticated, redirect to home
-		redirect('/');
-	}
+	// const status = await getServerAuthSession();
+	// console.log(status);
+	// if (!status) {
+	// 	// User unauthenticated, redirect to home
+	// 	redirect('/');
+	// }
 	return (
 		<div className=" flex items-center justify-center text-black">
 			<h1 className=" text-[3rem]">Guide page</h1>
