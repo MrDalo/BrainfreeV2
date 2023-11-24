21dbd666258e2c64fa7b0345557f9c6b9432add6
Pavel Hurdálek <pajahurdalek@seznam.cz>
2023-11-24 18:14:20 +0100

    feat: Full dashboard layout

--- src/app/components/dashboard/dashboard.tsx
+++ src/app/components/dashboard/dashboard.tsx
@@ -40,7 +40,9 @@ const Dashboard = ({ tasks }: { tasks: Task[] }) => {
 			<div className="flex w-full flex-col items-center justify-center gap-[4rem] p-[2rem]">
 				<div className="flex h-fit w-full items-center justify-between">
 					<div className="flex h-full w-[30%] flex-col items-center justify-center rounded-[1rem] bg-primary-green px-1 py-2">
-						<h3 className="text-[1.5rem] font-light">Tasks to prioritize</h3>
+						<h3 className="text-center text-[1.5rem] font-light">
+							Tasks to prioritize
+						</h3>
 						<p className="my-[-0.5rem] text-[2.5rem]">
 							{
 								todos.filter(x => x.priority === TaskPriority.NOT_ASSIGNED)
@@ -49,13 +51,17 @@ const Dashboard = ({ tasks }: { tasks: Task[] }) => {
 						</p>
 					</div>
 					<div className="flex h-full w-[30%] flex-col items-center justify-center rounded-[1rem] bg-primary-black px-1 py-2 text-primary-green">
-						<h3 className="text-[1.5rem] font-light">Tasks to complete</h3>
+						<h3 className="text-center text-[1.5rem] font-light">
+							Tasks to complete
+						</h3>
 						<p className="my-[-0.5rem] text-[2.5rem]">
 							{todos.filter(x => x.completed === false).length}
 						</p>
 					</div>
 					<div className="flex h-full w-[30%] flex-col items-center justify-center rounded-[1rem] bg-primary-green px-1 py-2">
-						<h3 className="text-[1.5rem] font-light">Tasks completed</h3>
+						<h3 className="text-center text-[1.5rem] font-light">
+							Tasks completed
+						</h3>
 						<p className="my-[-0.5rem] text-[2.5rem]">
 							{todos.filter(x => x.completed === true).length}
 						</p>
@@ -63,7 +69,16 @@ const Dashboard = ({ tasks }: { tasks: Task[] }) => {
 				</div>
 				<DnDContext onDragEnd={handleDragAndDrop}>
 					<div className="grid w-[95%] grid-cols-2 gap-[1px] rounded-[1.5rem] bg-primary-green font-light text-white">
-						<div className="h-[30vh] w-full rounded-tl-[1.5rem] bg-primary-black p-2">
+						<div className="relative h-[30vh] w-full rounded-tl-[1.5rem] bg-primary-black p-2">
+							<h2 className="tratext-center absolute left-[50%] top-[50%] translate-x-[-50%] translate-y-[-50%] text-[2.5rem] font-light leading-[2.5rem] text-white">
+								Do
+							</h2>
+							<p className="absolute left-[50%] top-[-2.5rem] translate-x-[-50%] text-[1.5rem] font-light text-primary-green">
+								Urgent
+							</p>
+							<p className="absolute left-[-1.5rem] top-[50%] translate-x-[-50%] translate-y-[-50%] -rotate-90 text-[1.5rem] font-light text-primary-green">
+								Important
+							</p>
 							<DroppableTodoField
 								droppableId={TaskPriority.URGENT_IMPORTANT}
 								droppableName="Do"
@@ -72,7 +87,13 @@ const Dashboard = ({ tasks }: { tasks: Task[] }) => {
 								)}
 							/>
 						</div>
-						<div className="h-[30vh] w-full rounded-tr-[1.5rem] bg-primary-black p-2">
+						<div className="relative h-[30vh] w-full rounded-tr-[1.5rem] bg-primary-black p-2">
+							<h2 className="tratext-center absolute left-[50%] top-[50%] translate-x-[-50%] translate-y-[-50%] text-[2.5rem] font-light leading-[2.5rem] text-white">
+								Schedule
+							</h2>
+							<p className="absolute left-[50%] top-[-2.5rem] translate-x-[-50%] text-[1.5rem] font-light text-primary-green">
+								Non urgent
+							</p>
 							<DroppableTodoField
 								droppableId={TaskPriority.NOT_URGENT_IMPORTANT}
 								droppableName="Schedule"
@@ -81,7 +102,13 @@ const Dashboard = ({ tasks }: { tasks: Task[] }) => {
 								)}
 							/>
 						</div>
-						<div className="h-[30vh] w-full rounded-bl-[1.5rem] bg-primary-black p-2">
+						<div className="relative h-[30vh] w-full rounded-bl-[1.5rem] bg-primary-black p-2">
+							<h2 className="tratext-center absolute left-[50%] top-[50%] translate-x-[-50%] translate-y-[-50%] text-[2.5rem] font-light leading-[2.5rem] text-white">
+								Delegate
+							</h2>
+							<p className="absolute left-[-1.5rem] top-[50%] translate-x-[-50%] translate-y-[-50%] -rotate-90 text-[1.5rem] font-light text-primary-green">
+								Non important
+							</p>
 							<DroppableTodoField
 								droppableId={TaskPriority.URGENT_NOT_IMPORTANT}
 								droppableName="Delegate"
@@ -90,7 +117,10 @@ const Dashboard = ({ tasks }: { tasks: Task[] }) => {
 								)}
 							/>
 						</div>
-						<div className="h-[30vh] w-full rounded-br-[1.5rem] bg-primary-black p-2">
+						<div className="relative h-[30vh] w-full rounded-br-[1.5rem] bg-primary-black p-2">
+							<h2 className="tratext-center absolute left-[50%] top-[50%] translate-x-[-50%] translate-y-[-50%] text-[2.5rem] font-light leading-[2.5rem] text-white">
+								Delete
+							</h2>
 							<DroppableTodoField
 								droppableId={TaskPriority.NOT_URGENT_NOT_IMPORTANT}
 								droppableName="Eliminate"
